7c00b1aa368fa4449be92eecaa8a61e21a3029ed
Kenneth <kenneth.l.thomsen@gmail.com>
2022-03-01 14:13:32 +0100

    Commit to pc

--- backend/achuus/models.py
+++ backend/achuus/models.py
@@ -1,20 +1,23 @@
 from django.db import models
-
-# Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
+
 
 class Achuu_Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     follows = models.ManyToManyField(
-        "self",
-        related_name="followd_by",
-        symmetrical = False, 
-        blank = True
+        "self", related_name="followed_by", symmetrical=False, blank=True
     )
-    #galleryID = models.IntegerField(blank=True, null=True)
-    #userID
-    
-    
 
-    
-    
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Achuu_Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.add(instance.profile)
+        user_profile.save()
