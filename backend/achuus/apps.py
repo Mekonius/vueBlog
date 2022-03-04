bcc9316a401523e8c2fad3d48bb5e1ee50e5feca
Kenneth Løvgren Thomsen <kenneth.l.thomsen@gmail.com>
2022-03-05 00:47:59 +0100

    commit things

--- backend/achuus/apps.py
+++ backend/achuus/apps.py
@@ -1,12 +1,11 @@
 from django.apps import AppConfig
 from django.core.signals import request_finished
 
-
-
 class AchuusConfig(AppConfig):
     name = 'achuus'
 
     def ready(self):
         from .import signals
+        
         
         request_finished.connect(signals.create_profile)
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 from django.conf.urls.static import static
@@ -8,6 +8,7 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("achuus.urls")),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 
